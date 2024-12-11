b4b0beec82c1e215e3761c954b125e88be16e37f
Kevin-Ocon <kevin.ocon@msw.com.mx>
2024-12-10 18:41:19 -0600

    Diseño

---
+++ src/data/servicesData.js
@@ -0,0 +1,30 @@
+import {FaShippingFast, FaShieldAlt, FaTags, FaCreditCard} from 'react-icons/fa';
+
+const servicesData = [
+    {
+        id: 1,
+        icon: <FaShippingFast />,
+        title: "Entrega Express",
+        info: "Llega en 24 Hours",
+    },
+    {
+        id: 2,
+        icon: <FaShieldAlt />,
+        title: "Brand Warranty",
+        info: "Productos 100% Originales",
+    },
+    {
+        id: 3,
+        icon: <FaTags />,
+        title: "Buenos Vendedores",
+        info: "En todas las ordenes",
+    },
+    {
+        id: 4,
+        icon: <FaCreditCard />,
+        title: "Seguridad de pago",
+        info: "Seguridad certificada",
+    },
+];
+
+export default servicesData;
